487ec565451210080895ddf46a9b4d94dbbb8490
Vlad Prus <pruvladik@gmail.com>
2024-10-16 19:45:58 +0300

    updates

--- src/lib/create-app.ts
+++ src/lib/create-app.ts
@@ -2,7 +2,7 @@ import { OpenAPIHono } from "@hono/zod-openapi";
 import { notFound, onError, serveEmojiFavicon } from "stoker/middlewares";
 import { defaultHook } from "stoker/openapi";
 import { pinoLogger } from "@/middlewares/pino-logger";
-
+import { cors } from 'hono/cors'
 import type { AppBindings, AppOpenAPI } from "./types";
 
 export function createRouter() {
@@ -16,6 +16,12 @@ export default function createApp() {
   const app = createRouter();
   app.use(serveEmojiFavicon("📝"));
   app.use(pinoLogger());
+  app.use('*', cors({
+    allowMethods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowHeaders: ['Content-Type', 'Authorization'],
+    exposeHeaders: ['Content-Length', 'X-Requested-With'],
+    origin: "*",
+  }))
   app.notFound(notFound);
   app.onError(onError);
   return app;
