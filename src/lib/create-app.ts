afb7455415464a7784109596bc8a3afd4f95867f
Vlad Prus <pruvladik@gmail.com>
2025-02-07 01:58:04 +0200

    fix cors?

--- src/lib/create-app.ts
+++ src/lib/create-app.ts
@@ -25,7 +25,12 @@ export default function createApp() {
       allowMethods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
       allowHeaders: ["Content-Type", "Authorization", "X-Requested-With"],
       exposeHeaders: ["Content-Length", "X-Requested-With"],
-      origin: ["http://127.0.0.1:5500", "http://127.0.0.1:3000"],
+      origin: [
+        "http://127.0.0.1:5500",
+        "http://127.0.0.1:3000",
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+      ],
       credentials: true,
     })
   );
